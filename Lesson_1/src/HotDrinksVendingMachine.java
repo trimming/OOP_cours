8a01dee6d90c1644e1aa6de2274f17cb49a43e9b
trimming <55543686+trimming@users.noreply.github.com>
2023-09-29 09:25:37 +0200

    Update HotDrinksVendingMachine.java

--- Lesson_1/src/HotDrinksVendingMachine.java
+++ Lesson_1/src/HotDrinksVendingMachine.java
@@ -1,11 +1,40 @@
 public class HotDrinksVendingMachine implements VendingMachine{
+    
+    List<HotDrink> drinksList;
+    
+    public List<HotDrink> getDrinksList() {
+        return drinksList;
+    }
 
+    public void setDrinksList(List<HotDrink> drinksList) {
+        this.drinksList = new ArrayList<>(drinksList);
+    }
+
+    public HotDrinksVendingMachine(List<HotDrink> drinksList){
+        this.drinksList = drinksList;
+    }
+
+    public HotDrink getProduct(String name, int volume, int temperature){
+        for(HotDrink hotDrink: drinksList){
+            if(hotDrink.getName().equals(name) && hotDrink.getVolume() == volume && hotDrink.getTemperature() == temperature){
+                return hotDrink;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString(){
+        return "HotDrinksVendingMachine{" +
+                "drinksList=" + drinksList +
+                '}';
+    }
 
     @Override
     public Product getProduct(String name) throws IllegalStateException {
-        for(Product product: productList){
-            if(product.getName().equals(name)){
-                return product;
+        for(Product drink: drinksList){
+            if(drink.getName().equals(name)){
+                return drink;
             }
         }
         return null;
