971ad6bdea261f7d4669283e40c009a59764aa8d
trimming <55543686+trimming@users.noreply.github.com>
2023-09-29 08:30:20 +0200

    Update HotDrinksVendingMachine.java

--- Lesson_1/src/HotDrinksVendingMachine.java
+++ Lesson_1/src/HotDrinksVendingMachine.java
@@ -3,6 +3,11 @@ public class HotDrinksVendingMachine implements VendingMachine{
 
     @Override
     public Product getProduct(String name) throws IllegalStateException {
+        for(Product product: productList){
+            if(product.getName().equals(name)){
+                return product;
+            }
+        }
         return null;
     }
 }
