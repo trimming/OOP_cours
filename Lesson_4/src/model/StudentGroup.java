538bbcac9dcb5f455bdbd54864a64a3b4d7b314f
Alexey Golikov <lexgolikoff@gmail.com>
2023-10-07 13:05:14 +0300

    create package model

---
+++ Lesson_4/src/model/StudentGroup.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.List;
+
+public class StudentGroup implements Iterable<Student>{
+    private List<Student> students;
+    private Teacher teacher;
+
+    public StudentGroup(List<Student> students, Teacher teacher) {
+        this.students = students;
+        this.teacher = teacher;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentGroup{" +
+                "students=" + students +
+                ", teacher=" + teacher +
+                '}';
+    }
+    @Override
+    public StudentGroupIterator iterator() {
+        return new StudentGroupIterator(this);
+    }
+}
